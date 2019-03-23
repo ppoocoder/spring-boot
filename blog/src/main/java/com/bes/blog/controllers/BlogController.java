027df38f633d87038c06efea638b590c2c3df350
youngwoo.noh <youngwoo.noh@bespinglobal.com>
2019-03-23 13:18:49 +0900

    spring-boot init

---
+++ blog/src/main/java/com/bes/blog/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package com.bes.blog.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+    
+    @GetMapping(value= "/")
+    public String index(){
+        return "index";
+    }
+}
